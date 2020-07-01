ad5966c142c563219a4e58cd0451d4ccea0e9412
trecho26 <hectorlara260698@gmail.com>
2020-07-01 15:46:41 -0600

    Docs agregado

---
+++ docs/precache-manifest.fe30c216517230aae22b5c1c16b133d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c253f70828ae95d742183ceaa002f61a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9870d489a4b84bde22ca",
+    "url": "/static/css/main.b9247c33.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9870d489a4b84bde22ca",
+    "url": "/static/js/main.67c78972.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
